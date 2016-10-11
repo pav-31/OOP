b2f9e5e4fd7f0e40197bc75a5f7eb04a31ce0fa1
pav-31 <favouritecolorisrain@gmail.com>
2016-10-11 14:46:24 +0300

    first commit

---
+++ Compare/Compare/Compare.cpp
@@ -0,0 +1,68 @@
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool CheckFileOpening(ifstream & file)
+{
+	if (!file.is_open())
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		cout << "Invalid arguments count" << endl
+			<< "Usage: copyfile.exe <input file1> <input file2>" << endl;
+		return 1;
+	}
+
+	ifstream file1(argv[1]);
+
+	if (!CheckFileOpening(file1))
+	{
+		cout << "Failed to open " << argv[1] << " for reading" << endl;
+		return 1;
+	}
+
+	ifstream file2(argv[2]);
+
+	if (!CheckFileOpening(file2))
+	{
+		cout << "Failed to open " << argv[2] << " for reading" << endl;
+		return 1;
+	}
+
+	string stringFromFile1;
+	string stringFromFile2;
+
+	int lineNumber = 1;
+
+	while ((!file1.eof()) || (!file2.eof()))
+	{
+		getline(file1, stringFromFile1);
+		getline(file2, stringFromFile2);
+
+		if (stringFromFile1 == stringFromFile2)
+		{
+			++lineNumber;
+		}
+		else
+		{
+			cout << "Files are different. Line number is " << lineNumber << endl;
+			return 1;
+		}
+
+	}
+	cout << "Files are equal" << endl;
+	return 0;
+}
---
+++ Compare/Compare/test.bat
@@ -0,0 +1,34 @@
+set PROGRAM="%~1"
+
+rem на входе 1 аргумент
+%PROGRAM% not_empty.txt
+if NOT ERRORLEVEL 1 goto err
+
+rem некорректно введено имя файла
+%PROGRAM% not_found.txt
+if NOT ERRORLEVEL 1 goto err
+
+rem проверяем сравнение двух файлов различных во второй строке
+%PROGRAM% difference_in_second_line1.txt difference_in_second_line2.txt
+if NOT ERRORLEVEL 1 goto err
+
+%PROGRAM% difference_in_second_line2.txt difference_in_second_line1.txt
+if NOT ERRORLEVEL 1 goto err
+
+rem сравнение двух файлов, когда в первом файле три строки, а во втором две
+%PROGRAM% three_lines.txt two_lines.txt                      
+if NOT ERRORLEVEL 1 goto err
+
+%PROGRAM% two_lines.txt three_lines.txt                      
+if NOT ERRORLEVEL 1 goto err
+
+rem сравнение не пустого файла с самим собой
+%PROGRAM% not_empty.txt not_empty.txt
+if ERRORLEVEL 1 goto err
+
+echo Program testing succeeded
+exit 0
+
+:err
+echo Program testing failed
+exit 1
